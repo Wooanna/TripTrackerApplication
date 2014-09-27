155501d66ba187071729693fbb1627ce149003cb
Yoanna Mareva <jo.thirdeyevision@gmail.com>
2014-09-27 16:06:46 -0500

    functionality of adding Trips to database added :)

--- Trip Tracker/scripts/view-models/driverMode-model.js
+++ Trip Tracker/scripts/view-models/driverMode-model.js
@@ -1,10 +1,42 @@
 var app = app || {};
 app.viewmodels = app.viewmodels || {};
 
-(function(scope){
+(function (scope) {
     scope.driverMode = kendo.observable({
         title: 'Start your trip here',
-        
+        tripTitle: '',
+        from: '',
+        to: '',
+        tripDescription: '',
+        getInfo: function () {
+            // console.log(this.get('tripTitle'));
+            //  console.log(this.get('from'));
+            //  console.log(this.get('to'));
+            //  console.log(this.get('tripDescription'));
+            var observableObject = new kendo.data.ObservableObject({
+                'TripTitle': this.get('tripTitle'),
+                'From' : this.get('from'),
+                'To': this.get('to'),
+                'Content' : this.get('tripDescription'),
+                
+            });
+             var applicationSettings = {
+        apiKey: 'DW6AEjXKXlIKroMU'
+    };
+    // initialize Icenium Everlive SDK
+    var el = new Everlive({
+        apiKey: applicationSettings.apiKey
     });
-    
+            var data = el.data('Trip');
+data.create(observableObject,
+    function(data){
+        alert(JSON.stringify(data));
+    },
+    function(error){
+        alert(JSON.stringify(error));
+    });
+        }
+
+    });
+
 }(app.viewmodels));
