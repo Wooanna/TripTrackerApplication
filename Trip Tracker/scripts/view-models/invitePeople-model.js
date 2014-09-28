4e77745c65c198755d7997f3b0e828aa30f6a327
Yoanna Mareva <jo.thirdeyevision@gmail.com>
2014-09-28 07:53:55 -0500

    Changes made by Yoanna Mareva (jo.thirdeyevision@gmail.com).

--- Trip Tracker/scripts/view-models/invitePeople-model.js
+++ Trip Tracker/scripts/view-models/invitePeople-model.js
@@ -4,7 +4,13 @@ app.viewmodels = app.viewmodels || {};
 (function(scope){
     scope.invitePeopleMode = kendo.observable({
         title: 'Invite people to watch your trip',
-        
+        contacts: [{
+            displayName: "ChichoGosho"
+        },
+                  {
+            displayName: "ChichoPesho"
+        }]
+                 
     });
     
 }(app.viewmodels));
