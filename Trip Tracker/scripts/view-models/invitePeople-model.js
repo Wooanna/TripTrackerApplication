adf2ffded9277256dfdd1343d06cd1af9a88a510
Rosica Mineva <rosicaboykova@gmail.com>
2014-09-28 08:17:19 -0500

    Invite people mode developed

--- Trip Tracker/scripts/view-models/invitePeople-model.js
+++ Trip Tracker/scripts/view-models/invitePeople-model.js
@@ -1,10 +1,31 @@
 var app = app || {};
 app.viewmodels = app.viewmodels || {};
 
-(function(scope){
+(function (scope) {
     scope.invitePeopleMode = kendo.observable({
         title: 'Invite people to watch your trip',
-        
+        contactList: [],
+        invite: function () {
+
+            var options = new ContactFindOptions();
+            
+            options.multiple = true;
+
+            var fields = [navigator.contacts.fieldType.id, navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
+            navigator.contacts.find(fields, onSuccess, onError, options);
+        }
+
     });
-    
+
+    //Extract all contacts
+    function onSuccess(contacts) {
+        alert('Found ' + contacts.length + ' contacts.');
+        scope.invitePeopleMode.contactList = contacts;
+    };
+
+
+    function onError(contactError) {
+        alert('Error: ' + JSON.stringify(contactError));
+    };
+
 }(app.viewmodels));
