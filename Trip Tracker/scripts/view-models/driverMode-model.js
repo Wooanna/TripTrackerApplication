eec73814bbd1bfaebef395e91113b9c77bc8520e
Yoanna Mareva <jo.thirdeyevision@gmail.com>
2014-09-29 01:50:05 -0500

    Checking network connection added. cordova API connection used. Changes made by Yoanna Mareva (jo.thirdeyevision@gmail.com).

--- Trip Tracker/scripts/view-models/driverMode-model.js
+++ Trip Tracker/scripts/view-models/driverMode-model.js
@@ -1,103 +1,115 @@
 var app = app || {};
 app.viewmodels = app.viewmodels || {};
 
-
-
-
 (function (scope) {
-    var applicationSettings = {
-        apiKey: 'DW6AEjXKXlIKroMU'
-    };
-    var el = new Everlive({
-        apiKey: applicationSettings.apiKey
-    });
+        var applicationSettings = {
+            apiKey: 'DW6AEjXKXlIKroMU'
+        };
+        var el = new Everlive({
+            apiKey: applicationSettings.apiKey
+        });
 
-    scope.driverMode = kendo.observable({
-        title: 'Start your trip here',
-        tripTitle: '',
-        from: '',
-        to: '',
-        tripDescription: '',
-        date: '',
-        tripType: '',
-        tripImage: '',
-        tripId: '3',
-        freePlaces: 0,
+        scope.driverMode = kendo.observable({
+                title: 'Start your trip here',
+                tripTitle: '',
+                from: '',
+                to: '',
+                tripDescription: '',
+                date: '',
+                tripType: '',
+                tripImage: '',
+                tripId: '3',
+                freePlaces: 0,
 
-        getInfo: function () {       
-              
-            var observableObject = new kendo.data.ObservableObject({
-                'TripTitle': this.get('tripTitle'),
-                'From': this.get('from'),
-                'To': this.get('to'),
-                'Content': this.get('tripDescription'),
-                'Date': this.get('date'),
-                'TripType': this.get('tripType'),
-                'TripImage': this.get('tripImage'),
-                'FreePlaces': this.get('freePlaces')
-            });
-           
+                getInfo: function () {
 
-            var data = el.data('Trip');
-             
-            data.create(observableObject, onSuccess, onError);
-            
-               function onSuccess (data) {
-                 //   alert(JSON.stringify(data));
-                scope.driverMode.tripId = data.result.Id;
-                 // console.log(scope.driverMode.tripId);       
-                };
-            
-            function onError(error) {
-                    alert(JSON.stringify(error));
-                };
-        },
+                    var observableObject = new kendo.data.ObservableObject({
+                        'TripTitle': this.get('tripTitle'),
+                        'From': this.get('from'),
+                        'To': this.get('to'),
+                        'Content': this.get('tripDescription'),
+                        'Date': this.get('date'),
+                        'TripType': this.get('tripType'),
+                        'TripImage': this.get('tripImage'),
+                        'FreePlaces': this.get('freePlaces')
+                    });
 
-        addImage: function () {
-            var success = function (data) {
-                var picture =  {
-                    Filename: Math.random().toString(36).substring(2, 15) + ".jpg",
-                    ContentType: "image/jpeg",
-                    base64: data
-                };
-             
-                el.Files.create(picture);
-                scope.driverMode.tripImage = picture.Filename;
-                 alert('The trip image successfully added.');
-            };
-            var error = function () {
-                navigator.notification.alert("Unfortunately we could not add the image");
-            };
-            var config = {
-                destinationType: Camera.DestinationType.DATA_URL,
-                targetHeight: 400,
-                targetWidth: 400
-            };
-            navigator.camera.getPicture(success, error, config);
-        }
-      
-        //
+                    function checkConnection() {
+                        var networkState = navigator.connection.type;
 
+                        var states = {};
+                        states[Connection.UNKNOWN] = 'Unknown connection';
+                        states[Connection.ETHERNET] = 'Ethernet connection';
+                        states[Connection.WIFI] = 'WiFi connection';
+                        states[Connection.CELL_2G] = 'Cell 2G connection';
+                        states[Connection.CELL_3G] = 'Cell 3G connection';
+                        states[Connection.CELL_4G] = 'Cell 4G connection';
+                        states[Connection.CELL] = 'Cell generic connection';
+                        states[Connection.NONE] = 'No network connection';
+                        if (states[networkState] == 'No network connection') {
+                            alert('Please check your device connection and try again.');
+                        } else {
 
-        //TODO
-        //function loadPhotos() {
-        //    everlive.Files.get().then(function (data) {
-        //        var files = [];
-        //        data.result.forEach(function (image) {
-        //            files.push(image.Uri);
-        //        });
-        //        $("#images").kendoMobileListView({
-        //            dataSource: files,
-        //            template: "<img src='#: data #'>"
-        //        });
-        //    });
-        //}
-        //
-        //
-    });
-    
-      
-       
+                            var data = el.data('Trip');
+                            data.create(observableObject, onSuccess, onError);
+
+                            function onSuccess(data) {
+                                scope.driverMode.tripId = data.result.Id;
+                            };
+
+                            function onError(error) {
+                                alert(JSON.stringify(error));
+                            };
+                        }
+                    }
+                        checkConnection();
+                    },
+
+                    addImage: function () {
+                        var success = function (data) {
+                            var picture = {
+                                Filename: Math.random().toString(36).substring(2, 15) + ".jpg",
+                                ContentType: "image/jpeg",
+                                base64: data
+                            };
+
+                            el.Files.create(picture);
+                            scope.driverMode.tripImage = picture.Filename;
+                            alert('The trip image successfully added.');
+                        };
+                        var error = function () {
+                            navigator.notification.alert("Unfortunately we could not add the image");
+                        };
+                        var config = {
+                            destinationType: Camera.DestinationType.DATA_URL,
+                            targetHeight: 400,
+                            targetWidth: 400
+                        };
+                        navigator.camera.getPicture(success, error, config);
+                    }
+
+                    //
 
 
-}(app.viewmodels));
+                    //TODO
+                    //function loadPhotos() {
+                    //    everlive.Files.get().then(function (data) {
+                    //        var files = [];
+                    //        data.result.forEach(function (image) {
+                    //            files.push(image.Uri);
+                    //        });
+                    //        $("#images").kendoMobileListView({
+                    //            dataSource: files,
+                    //            template: "<img src='#: data #'>"
+                    //        });
+                    //    });
+                    //}
+                    //
+                    //
+                });
+
+
+
+
+
+        }(app.viewmodels));
--- Trip Tracker/scripts/view-models/watchMode-model.js
+++ Trip Tracker/scripts/view-models/watchMode-model.js
@@ -3,8 +3,24 @@ app.viewmodels = app.viewmodels || {};
 
 (function (scope) {
     function getTrips(e) {
-  
-        var applicationSettings = {
+        function checkConnection() {
+            var networkState = navigator.connection.type;
+
+            var states = {};
+            states[Connection.UNKNOWN] = 'Unknown connection';
+            states[Connection.ETHERNET] = 'Ethernet connection';
+            states[Connection.WIFI] = 'WiFi connection';
+            states[Connection.CELL_2G] = 'Cell 2G connection';
+            states[Connection.CELL_3G] = 'Cell 3G connection';
+            states[Connection.CELL_4G] = 'Cell 4G connection';
+            states[Connection.CELL] = 'Cell generic connection';
+            states[Connection.NONE] = 'No network connection';
+            if (states[networkState] == 'No network connection') {
+                alert('Please check your device connection and try again.');
+            }
+            else
+            {
+                 var applicationSettings = {
             apiKey: 'DW6AEjXKXlIKroMU'
         };
         var el = new Everlive({
@@ -21,13 +37,19 @@ app.viewmodels = app.viewmodels || {};
                     };
                     var vm = kendo.observable(model);
                     kendo.bind(e.view.element, vm);
-                        console.log(model.trips);
+                    console.log(model.trips);
                     alert(JSON.stringify(data));
                 },
                 function (error) {
                     alert(JSON.stringify(error));
-                }); 
+                });
 
+            }
+        }
+
+         checkConnection();
+
+       
 
     }
 
